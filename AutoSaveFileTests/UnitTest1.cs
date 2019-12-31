1330a5518028d2dfd3b048d7661070c9626f08df
Hangjit Rai <rai.hangjit@gmail.com>
2019-12-31 12:39:37 +1100

    FILE-TYPES FILE-TYPES Added Tests Plumbing

---
+++ AutoSaveFileTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace AutoSaveFileTests
+{
+    public class AutoSaveFilePackageTests
+    {
+        [TestCase]
+        public void GetFileType()
+        {
+            var i = 8;
+            i.Should().Be(1);
+        }
+    }
+}
