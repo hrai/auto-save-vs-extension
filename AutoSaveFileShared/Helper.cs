9ababc1397b3a6a058bae5091396432613bbd0ef
Hangjit Rai <hangjit.rai@outlook.com>
2021-09-28 13:42:14 +1000

    EXCLUDED-FOLDERS Added Excluded Folders Config

--- AutoSaveFileShared/Helper.cs
+++ AutoSaveFileShared/Helper.cs
@@ -2,6 +2,11 @@
 using EnvDTE;
 using System.Runtime.CompilerServices;
 using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System;
 
 [assembly: InternalsVisibleTo("AutoSaveFileTests")]
 namespace AutoSaveFile
@@ -25,8 +30,15 @@ namespace AutoSaveFile
         {
             var windowType = window.Kind;
 
+            
             if (windowType == "Document")
             {
+                var directoryList = GetConstituentFoldersFromPath(window);
+                if (!IsDocumentInIgnoredFolder(optionsPage.IgnoredFolders, directoryList))
+                {
+                    return true;
+                }
+
                 var fileType = GetFileType(window);
                 var ignoredFileTypes = optionsPage.IgnoredFileTypes?
                     .ToLowerInvariant()
@@ -45,6 +57,62 @@ namespace AutoSaveFile
             return false;
         }
 
+        private IList<string> GetConstituentFoldersFromPath(Window window)
+        {
+            var documentFullName = window.Document?.FullName;
 
+            if (documentFullName == null)
+                documentFullName = window.Project?.FullName;
+
+            return documentFullName.Split(new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar });
+        }
+
+        private bool IsDocumentInIgnoredFolder(string ignoredFolders, IList<string> directoryList)
+        {
+            foreach (string folder in ignoredFolders.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (!DirectoryHasPermission(Path.GetDirectoryName(folder), FileSystemRights.Write))
+                    return false;
+
+                var incl = folder.StartsWith("!");
+                var cleanFolderName = folder.Substring(incl ? 1 : 0).Trim();
+
+                cleanFolderName = cleanFolderName.Replace('\\', '/');
+                cleanFolderName = Regex.Escape(cleanFolderName);
+
+                return directoryList.Any(dirs => dirs.Contains(cleanFolderName));
+            }
+
+            return false;
+        }
+
+        private static bool DirectoryHasPermission(string DirectoryPath, FileSystemRights AccessRight)
+        {
+            if (string.IsNullOrEmpty(DirectoryPath))
+                return false;
+
+            try
+            {
+                var rules = Directory.GetAccessControl(DirectoryPath).GetAccessRules(true, true, typeof(SecurityIdentifier));
+                var identity = WindowsIdentity.GetCurrent();
+
+                foreach (FileSystemAccessRule rule in rules)
+                {
+                    if (!identity.Groups.Contains(rule.IdentityReference))
+                        continue;
+
+                    if ((AccessRight & rule.FileSystemRights) != AccessRight)
+                        continue;
+
+                    if (rule.AccessControlType != AccessControlType.Allow)
+                        continue;
+
+                    return true;
+                }
+            }
+            catch { }
+
+            return false;
+        }
     }
 }
