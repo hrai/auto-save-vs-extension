59bf2ac86b1c37cfc23b36bbcfcd58e0de397bd9
Hangjit Rai <hangjit.rai@outlook.com>
2021-09-28 13:49:06 +1000

    EXCLUDED-FOLDERS Cleanup

--- AutoSaveFileShared/Helper.cs
+++ AutoSaveFileShared/Helper.cs
@@ -2,8 +2,6 @@
 using EnvDTE;
 using System.Runtime.CompilerServices;
 using System.Linq;
-using System.Security.AccessControl;
-using System.Security.Principal;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System;
@@ -29,15 +27,12 @@ namespace AutoSaveFile
         internal bool ShouldSaveDocument(Window window, OptionPageGrid optionsPage)
         {
             var windowType = window.Kind;
-
-            
+                        
             if (windowType == "Document")
             {
                 var directoryList = GetConstituentFoldersFromPath(window);
-                if (!IsDocumentInIgnoredFolder(optionsPage.IgnoredFolders, directoryList))
-                {
-                    return true;
-                }
+                if (IsDocumentInIgnoredFolder(optionsPage.IgnoredFolders, directoryList))
+                    return false;
 
                 var fileType = GetFileType(window);
                 var ignoredFileTypes = optionsPage.IgnoredFileTypes?
@@ -49,9 +44,7 @@ namespace AutoSaveFile
                     return true;
 
                 if (ignoredFileTypes != null && !ignoredFileTypes.Contains(fileType))
-                {
                     return true;
-                }
             }
 
             return false;
@@ -71,9 +64,6 @@ namespace AutoSaveFile
         {
             foreach (string folder in ignoredFolders.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
             {
-                if (!DirectoryHasPermission(Path.GetDirectoryName(folder), FileSystemRights.Write))
-                    return false;
-
                 var incl = folder.StartsWith("!");
                 var cleanFolderName = folder.Substring(incl ? 1 : 0).Trim();
 
@@ -82,35 +72,6 @@ namespace AutoSaveFile
 
                 return directoryList.Any(dirs => dirs.Contains(cleanFolderName));
             }
-
-            return false;
-        }
-
-        private static bool DirectoryHasPermission(string DirectoryPath, FileSystemRights AccessRight)
-        {
-            if (string.IsNullOrEmpty(DirectoryPath))
-                return false;
-
-            try
-            {
-                var rules = Directory.GetAccessControl(DirectoryPath).GetAccessRules(true, true, typeof(SecurityIdentifier));
-                var identity = WindowsIdentity.GetCurrent();
-
-                foreach (FileSystemAccessRule rule in rules)
-                {
-                    if (!identity.Groups.Contains(rule.IdentityReference))
-                        continue;
-
-                    if ((AccessRight & rule.FileSystemRights) != AccessRight)
-                        continue;
-
-                    if (rule.AccessControlType != AccessControlType.Allow)
-                        continue;
-
-                    return true;
-                }
-            }
-            catch { }
 
             return false;
         }
